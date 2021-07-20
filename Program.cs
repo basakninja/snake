5b38d3a3d7ffa73de03930332a4a5ef6d349218f
Uladzimir Basak <uladzimir.basak@softeq.com>
2021-07-20 14:06:05 +0300

    class point

--- Program.cs
+++ Program.cs
@@ -7,31 +7,24 @@ namespace snake
         static void Main(string[] args)
         {
 
-            int x1 = 5;
-            int y1 = 5;
-            char sym1 = '*';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '$';
+            p1.Draw();
 
-            Draw(x1, y1, sym1);
+            Point p2 = new Point();
 
-            x1 = 10;
-            y1 = 15;
-            sym1 = '*';
+            p2.x = 3;
+            p2.y = 7;
+            p2.sym = '%';
 
-            Draw(x1, y1, sym1);
+            p2.Draw();
 
-            x1 = 50;
-            y1 = 50;
-            sym1 = '*';
-
-            Draw(x1, y1, sym1);
 
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+        
     }
 }
